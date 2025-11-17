4e29d8cf892d41803512b21f28ce26ad7387d80b
RodrigoLozano <89640436+RodLozano@users.noreply.github.com>
2025-11-17 23:48:11 +0100

    añadir deploy

--- scripts/register_model.py
+++ scripts/register_model.py
@@ -1,3 +1,4 @@
+
 import mlflow
 from mlflow.tracking import MlflowClient
 import os
@@ -18,10 +19,11 @@ model_name =  os.getenv("MODEL_NAME", "no_name")
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 MODEL_DIR = PROJECT_ROOT / "models"
 model_artifact_path = MODEL_DIR / "model.pkl"
-
+print(model_artifact_path)
+print(run_id)
 # Register the model
 result = mlflow.register_model(
-    model_uri=f"runs:/{run_id}/{model_artifact_path}",
+    model_uri=f"runs:/{run_id}/model",
     name=model_name
 )
 
